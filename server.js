624d6094014b4bc0a496ad4e79a3579f2d529ca9
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-12-28 09:34:10 -0600

    deploy changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sports-app'));
+app.use(express.static(__dirname + '/dist/SportsApp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/sports-app/index.html'));});
+'/dist/SportsApp/index.html'));});
 app.listen(process.env.PORT || 8080);
